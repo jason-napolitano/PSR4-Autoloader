c32a07c703b81c0420c760886a948656234a0957
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2020-01-14 23:12:44 -0800

    Update Autoloader.php

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -10,26 +10,30 @@ abstract class Autoloader
      *
      * @var array $psr4
      */
-    public static $psr4 = [
-        'VendorNamepsace' => __DIR__ . '/src'
+    public static array $psr4 = [
+        'VendorNamepsace' => __DIR__ . '/src',
     ];
+
     /**
      * PSR4 classmap array
      *
      * @var array $classmap
      */
-    public static $classmap = [
+    public static array $classmap = [
         // ...
     ];
+
     /**
      * Map of class name prefixes
      *
      * @var array
      */
-    public static $prefixes = [
+    public static array $prefixes = [
         // ...
     ];
+
     // --------------------------------------------------------------------
+
     /**
      * Register the autoloader functionality
      *
@@ -42,20 +46,22 @@ abstract class Autoloader
 
         // Iterate through the PSR4 namespace array
         $namespaces = array_filter(self::$psr4);
-        if (!empty($namespaces)) {
-            foreach (self::$psr4 as $psr4 => $path) {
+        if ( ! empty($namespaces) ) {
+            foreach ( self::$psr4 as $psr4 => $path ) {
                 self::addNamespace($psr4, $path);
             }
         }
         // Iterate through the PSR4 classmap array
         $classmap = array_filter(self::$classmap);
-        if (!empty($classmap)) {
-            foreach (self::$classmap as $class) {
+        if ( ! empty($classmap) && class_exists($classmap) ) {
+            foreach ( self::$classmap as $class ) {
                 self::requireFiles($class . '.php');
             }
         }
     }
+
     // --------------------------------------------------------------------
+
     /**
      * Adds a namespace to the autoloader
      *
@@ -72,21 +78,23 @@ abstract class Autoloader
         // normalize the base directory with a trailing separator
         $base_dir = rtrim($base_dir, DIRECTORY_SEPARATOR) . '/';
         // initialize the namespace prefix array
-        if (isset(self::$prefixes[$prefix]) === false) {
+        if ( isset(self::$prefixes[$prefix]) === false ) {
             self::$prefixes[$prefix] = [];
         }
         // retain the base directory for the namespace prefix
-        if ($prepend) {
+        if ( $prepend ) {
             array_unshift(self::$prefixes[$prefix], $base_dir);
         } else {
             self::$prefixes[$prefix][] = $base_dir;
         }
     }
+
     // --------------------------------------------------------------------
+
     /**
      * Loads a class from the autoloader
      *
-     * @param  string $class
+     * @param string $class
      *
      * @return bool|string
      */
@@ -94,60 +102,65 @@ abstract class Autoloader
     {
         // the current namespace prefix
         $prefix = $class;
-        while (false !== $pos = strrpos($prefix, '\\')) {
+        while ( false !== $pos = strrpos($prefix, '\\') ) {
             // retain the trailing namespace separator in the prefix
             $prefix = substr($class, 0, $pos + 1);
             // the rest is the relative class name
             $relative_class = substr($class, $pos + 1);
             // try to load a mapped file for the prefix and relative class
             $mapped_file = self::loadMappedFile($prefix, $relative_class);
-            if ($mapped_file) {
+            if ( $mapped_file ) {
                 return $mapped_file;
             }
             $prefix = rtrim($prefix, '\\');
         }
         return false;
     }
+
     // --------------------------------------------------------------------
+
     /**
      * Loads a mapped file from the autoloader
      *
-     * @param  string $prefix
-     * @param  string $relative_class
+     * @param string $prefix
+     * @param string $relative_class
      *
      * @return bool|string
      */
     protected static function loadMappedFile(string $prefix, string $relative_class)
     {
         // are there any base directories for this namespace prefix?
-        if (isset(self::$prefixes[$prefix]) === false) {
+        if ( isset(self::$prefixes[$prefix]) === false ) {
             return false;
         }
-        foreach (self::$prefixes[$prefix] as $base_dir) {
+        foreach ( self::$prefixes[$prefix] as $base_dir ) {
             $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
             // if the mapped file exists, require it
-            if (self::requireFiles($file)) {
+            if ( self::requireFiles($file) ) {
                 // yes, we're done
                 return $file;
             }
         }
         return false;
     }
+
     // --------------------------------------------------------------------
+
     /**
      * Require a file or an array of files
      *
-     * @param  string $files
+     * @param string $files
      *
      * @return bool
      */
     public static function requireFiles(string $files): bool
     {
-        if (file_exists($files)) {
+        if ( file_exists($files) ) {
             require $files;
             return true;
         }
         return false;
     }
+
     // --------------------------------------------------------------------
 }
